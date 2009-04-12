f632a49227058cf5b363123957613453f40b9ceb
tallshort <tallshort@812eee4e-0d6c-11de-b5bf-434079a3519f>
2009-04-12 13:09:35 +0000

    Add documentation

--- trunk/projects/NLP_Project_DataPreProcessing/src/MaxEntClassifier.java
+++ trunk/projects/NLP_Project_DataPreProcessing/src/MaxEntClassifier.java
@@ -7,18 +7,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MaxEntClassifier {
-    
+
     private double globalRightNum;
     private double globalTotalSum;
     private ArrayList<Double> globalAccuarcyList = new ArrayList<Double>();
 
+    private double g = 20;
+
     public static void main(String[] args) {
         int startOffset = -1;
         int endOffset = 2;
-        int includeTokenPreOffset = 1;
+        int includeTokenPreOffset = -1;
         int includeTokenPostOffset = 3;
+        double g = 20;
         
         MaxEntClassifier c = new MaxEntClassifier();
+        c.setG(g);
+        
         // Verbs
         List<String> wordList = getVerbWordList();
         c.runMaxEnt(wordList, startOffset, endOffset, includeTokenPreOffset,
@@ -27,71 +32,88 @@ public class MaxEntClassifier {
         wordList = getNounWordList();
         startOffset = -1;
         endOffset = 1;
-        includeTokenPreOffset = 1;
+        includeTokenPreOffset = -1;
         includeTokenPostOffset = 2;
-        c.runMaxEnt(wordList, startOffset, endOffset, includeTokenPreOffset,
+         c.runMaxEnt(wordList, startOffset, endOffset, includeTokenPreOffset,
                 includeTokenPostOffset);
-        
+
         c.summarize();
     }
-    
-    private void summarize() {
+
+    /**
+     * 输出微平均和宏平均等统计信息
+     */
+    public void summarize() {
         System.out.println("==============================================");
         double sum = 0;
         for (Double accuracy : this.globalAccuarcyList) {
             sum = sum + accuracy;
         }
-        System.out.println("Global macro average: " + sum / globalAccuarcyList.size());
-        System.out.println("Global right sum: " + this.globalRightNum + " " + "Global total sum: " + this.globalTotalSum);
-        System.out.println("Global micro average: " + this.globalRightNum / this.globalTotalSum);
+        System.out.println("Global macro average: " + sum
+                / globalAccuarcyList.size());
+        System.out.println("Global right sum: " + this.globalRightNum + " "
+                + "Global total sum: " + this.globalTotalSum);
+        System.out.println("Global micro average: " + this.globalRightNum
+                / this.globalTotalSum);
         System.out.println("==============================================");
     }
 
-    private void runMaxEnt(List<String> wordList, int startOffset,
+    /**
+     * 按照给定参数运行MaxEnt，并记录下统计信息
+     * 
+     * @param wordList
+     *            待消歧词列表
+     * @param startOffset
+     *            相对于待消歧词的POS起始偏移量
+     * @param endOffset
+     *            相对于待消歧词的POS结尾偏移量
+     * @param includeTokenPreOffset
+     *            相对于POS起始偏移量的TOKEN起始偏移量
+     * @param includeTokenPostOffset
+     *            相对于POS起始偏移量的TOKEN结尾偏移量
+     */
+    public void runMaxEnt(List<String> wordList, int startOffset,
             int endOffset, int includeTokenPreOffset, int includeTokenPostOffset) {
         int iteration = 100;
         double rightNum = 0; // 微平均 记录特定词正确的个数
-        double totalNum = 0; // 微平均 记录特定词总共的个数 
+        double totalNum = 0; // 微平均 记录特定词总共的个数
         double rightSum = 0; // 微平均 记录正确的个数
         double totalSum = 0; // 微平均 记录总共的个数
         ArrayList<Double> accuarcyList = new ArrayList<Double>();
 
         for (String word : wordList) {
             try {
-                String postfix = startOffset + "_" + endOffset + "_" + includeTokenPreOffset + "_" + includeTokenPostOffset + "_" + word
-                        + ".txt";
+                String postfix = startOffset + "_" + endOffset + "_"
+                        + includeTokenPreOffset + "_" + includeTokenPostOffset
+                        + "_" + word + ".txt";
                 String train = "Chinese_train_pos.xml_" + postfix;
                 String test = "Chinese_test_pos.xml_" + postfix;
                 String model = "model_" + postfix;
                 String out = "out_" + postfix;
 
                 double maxAccuracy = -1;
-                out: for (double g = 20; g <= 20; g += 1) {
-                    Process p = Runtime.getRuntime().exec(
-                            "maxent -i " + iteration + " -g " + g + " -m "
-                                    + model + " -o " + out + " " + train + " "
-                                    + test);
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(p.getInputStream()));
+                Process p = Runtime.getRuntime().exec(
+                        "maxent -i " + iteration + " -g " + g + " -m " + model
+                                + " -o " + out + " " + train + " " + test);
+                BufferedReader in = new BufferedReader(new InputStreamReader(p
+                        .getInputStream()));
 
-                    String strProc = null;
-                    Pattern pattern = Pattern
-                            .compile("Accuracy:(.+)% [(](\\d+)/(\\d+)[)]");
-                    while ((strProc = in.readLine()) != null) {
-                        Matcher matcher = pattern.matcher(strProc);
-                        if (matcher.find()) {
-                            double accuracy = Double.parseDouble(matcher
-                                    .group(1));
-                            rightNum = Double.parseDouble(matcher.group(2));
-                            totalNum = Double.parseDouble(matcher.group(3));
-                            if (accuracy > maxAccuracy) {
-                                maxAccuracy = accuracy;
-                            }
-                            // System.out.println(strProc);
+                String strProc = null;
+                Pattern pattern = Pattern
+                        .compile("Accuracy:(.+)% [(](\\d+)/(\\d+)[)]");
+                while ((strProc = in.readLine()) != null) {
+                    Matcher matcher = pattern.matcher(strProc);
+                    if (matcher.find()) {
+                        double accuracy = Double.parseDouble(matcher.group(1));
+                        rightNum = Double.parseDouble(matcher.group(2));
+                        totalNum = Double.parseDouble(matcher.group(3));
+                        if (accuracy > maxAccuracy) {
+                            maxAccuracy = accuracy;
                         }
+                        // System.out.println(strProc);
                     }
-                    in.close();
                 }
+                in.close();
                 rightSum += rightNum;
                 this.globalRightNum += rightNum;
                 totalSum += totalNum;
@@ -109,14 +131,17 @@ public class MaxEntClassifier {
             sum = sum + accuracy;
         }
         System.out.println("==============================================");
-        System.out.println("Feature: " + startOffset + endOffset + includeTokenPreOffset + includeTokenPostOffset);
+        System.out.println("G: " + g); 
+        System.out.println("Feature: " + startOffset + endOffset
+                + includeTokenPreOffset + includeTokenPostOffset);
         System.out.println("Macro average: " + sum / wordList.size());
-        System.out.println("Right sum: " + rightSum + " " + "Total sum: " + totalSum);
+        System.out.println("Right sum: " + rightSum + " " + "Total sum: "
+                + totalSum);
         System.out.println("Micro average: " + rightSum / totalSum);
         System.out.println("==============================================");
     }
-    
-    private static List<String> getVerbWordList() {
+
+    public static List<String> getVerbWordList() {
         List<String> wordList = new ArrayList<String>();
         wordList.add("补");
         wordList.add("成立");
@@ -141,8 +166,8 @@ public class MaxEntClassifier {
         wordList.add("震惊");
         return wordList;
     }
-    
-    private static List<String> getNounWordList() {
+
+    public static List<String> getNounWordList() {
         List<String> wordList = new ArrayList<String>();
         wordList.add("本");
         wordList.add("表面");
@@ -164,6 +189,19 @@ public class MaxEntClassifier {
         wordList.add("眼光");
         wordList.add("中医");
         return wordList;
+    }
+
+    public double getG() {
+        return g;
+    }
+
+    /**
+     * 设置Gaussian prior的取值
+     * 
+     * @param g
+     */
+    public void setG(double g) {
+        this.g = g;
     }
 
 }
--- trunk/projects/NLP_Project_DataPreProcessing/src/Processor.java
+++ trunk/projects/NLP_Project_DataPreProcessing/src/Processor.java
@@ -6,37 +6,36 @@ public class Processor {
     public static void main(String[] args) {
         DataProcessing dp = new DataProcessing();
         // For maxent
-        dp.setClassLabelFirst(true);
-        dp.setSeparator(" ");
-        dp.setOnefilePerItem(true);
-        dp.setIncludeTokenPreOffset(1);
-        dp.setIncludeTokenPostOffset(3);
+//        dp.setClassLabelFirst(true);
+//        dp.setSeparator(" ");
+//        dp.setOnefilePerItem(true);
+        dp.setIncludeTokenPreOffset(0);
+        dp.setIncludeTokenPostOffset(0);
         dp.setIncludeToken(true);
         dp.setConsiderPunctuation(true);
         dp.setIncludeIgnoreValue(true);
         dp.setEmphasizeNeighborhoodToken(false);
-        // dp.setTargetWord("³ö");
         
-//        for (int tokenPreOffset = 1; tokenPreOffset <= 3; tokenPreOffset++) {
+//        for (int tokenPreOffset = -3; tokenPreOffset <= -1; tokenPreOffset++) {
 //            for (int tokenPostOffset = 1; tokenPostOffset <= 4; tokenPostOffset++) {
 //                dp.setIncludeTokenPreOffset(tokenPreOffset);
 //                dp.setIncludeTokenPostOffset(tokenPostOffset);
-////                for (int i = -3; i <= 0; i++) {
-////                    for (int j = 0; j <= 3; j++) {
-////                        // At least one tokens as the features
-////                        if (Math.abs(i - j) >= 1) {
-////                            generateData(dp, i, j);
-////                        }
-////                    }
-////                }
-////                generateData(dp, -1, 1);
-////                generateData(dp, -1, 2);
-////                generateData(dp, -2, 1);
-////                generateData(dp, -2, 2);
-////                generateData(dp, -1, 3);
-//            }
-//        }
-        generateData(dp, -1, 2);
+//                for (int i = -3; i <= 0; i++) {
+//                    for (int j = 0; j <= 3; j++) {
+//                        // At least one tokens as the features
+//                        if (Math.abs(i - j) >= 1) {
+//                            generateData(dp, i, j);
+//                        }
+//                    }
+//                }
+//                generateData(dp, -1, 1);
+//                generateData(dp, -1, 2);
+//                generateData(dp, -2, 1);
+//                generateData(dp, -2, 2);
+                generateData(dp, -1, 2);
+        //    }
+        //}
+        // generateData(dp, -1, 2);
         // System.out.println(dp.getTotalWordSet());
     }
 
