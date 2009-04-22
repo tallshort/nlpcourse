814178f5477c6c66c7af3aa1b6d3cbd5cc2a68c5
tallshort <tallshort@812eee4e-0d6c-11de-b5bf-434079a3519f>
2009-04-22 16:05:26 +0000

    Add textanalysis package

--- trunk/projects/NLP_Project2/src/weps/test/Main.java
+++ trunk/projects/NLP_Project2/src/weps/test/Main.java
@@ -12,14 +12,14 @@ public class Main {
     }
 
     private static void testExtractor() {
-        AbstractBodyExtractor extractor = new PythonBodyExtractor("extractor.py", 0.6);
+        AbstractBodyExtractor extractor = new PythonBodyExtractor("asset/extractor.py", 0.6);
         String body = extractor.extractBody("fixture/test1.html");
         System.out.println(body);
         extractor.setDatasetDir("weps2007/test");
         List<String> nameList = extractor.getPeopleNameList();
         System.out.println(nameList);
         System.out.println(nameList.size());
-        //extractor.setDatasetDir("fixture");
+        extractor.setDatasetDir("fixture");
         extractor.setBodiesTargetDir("test_webpages_bodies");
         extractor.extractBodies();
     }
