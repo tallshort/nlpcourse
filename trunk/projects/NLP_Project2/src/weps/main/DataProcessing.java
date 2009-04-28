a094fc9b7bd72d15dde21bcb2ba06a44809171a2
tallshort <tallshort@812eee4e-0d6c-11de-b5bf-434079a3519f>
2009-04-28 11:00:27 +0000

    add ClutoClusterXMLGenerator

--- trunk/projects/NLP_Project2/src/weps/main/DataProcessing.java
+++ trunk/projects/NLP_Project2/src/weps/main/DataProcessing.java
@@ -1,18 +1,29 @@
 package weps.main;
 
+import weps.ClutoClusterXMLGenerator;
 import weps.ClutoRawDataExtractor;
 
 public class DataProcessing {
     public static void main(String[] args) throws Exception {
-        buildDataForCluto();
+        // buildDataForCluto();
+        gererateClusterXMLs();
     }
 
     private static void buildDataForCluto() throws Exception {
         ClutoRawDataExtractor extractor = new ClutoRawDataExtractor();
-        // extractor.setTargetPerson("Alvin_Cooper");
+        extractor.setTargetPerson("William_Dickson");
         extractor.setTargetDir("clutoRawData");//写到哪里
         extractor.setBodyDir("test_webpages_bodies_version_1");//源文件
         extractor.setUrlDir("url_data");  //URL文件
         extractor.extractContents();
+    }
+    
+    private static void gererateClusterXMLs() throws Exception {
+        ClutoClusterXMLGenerator generator = new ClutoClusterXMLGenerator();
+        generator.setClutoClusterResultsDir("cluto_result2/matrixFile");
+        // generator.setTargetPerson("Alvin_Cooper");
+        generator.setXmlResultsDir("cluto_xml_result/cluto_xml_result2");
+        generator.setClusterNum(40);
+        generator.gererateClusterXMLs();
     }
 }
