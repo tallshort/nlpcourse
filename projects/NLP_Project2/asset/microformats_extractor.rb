13cab3f176b4010702f4a49e0943688c3d4b2d65
tallshort <tallshort@812eee4e-0d6c-11de-b5bf-434079a3519f>
2009-05-05 15:26:33 +0000

    Add microformats Add dynamic hier Clusterer

--- projects/NLP_Project2/asset/microformats_extractor.rb
+++ projects/NLP_Project2/asset/microformats_extractor.rb
@@ -1,4 +1,17 @@
 require 'mofo'
 
+def print_card(card)
+  puts card.title if card.properties.member?("title")
+  puts card.locality if card.properties.member?("locality")
+  puts card.org if card.properties.member?("org")
+end
+
 card = hCard.find ARGV[0]
-p card
+
+if card.respond_to?:each
+  card.each do |c|
+    print_card(c)
+  end
+else
+  print_card(card)
+end
