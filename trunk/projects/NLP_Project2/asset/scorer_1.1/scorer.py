63e333ff549fd60ba5017bd03f887d3355e3abdb
tallshort <tallshort@812eee4e-0d6c-11de-b5bf-434079a3519f>
2009-05-05 15:26:33 +0000

    Add microformats Add dynamic hier Clusterer

--- trunk/projects/NLP_Project2/asset/scorer_1.1/scorer.py
+++ trunk/projects/NLP_Project2/asset/scorer_1.1/scorer.py
@@ -1,2 +1,2 @@
 import os
-os.system("java -cp asset/scorer_1.1/wepsEvaluation.jar es.nlp.uned.weps.evaluation.SystemScorer weps2007/test/truth_files/official_annotation cluto_xml_result result -ALLMEASURES -AllInOne -OneInOne -Combined -average")
+os.system("java -cp asset/scorer_1.1/wepsEvaluation.jar es.nlp.uned.weps.evaluation.SystemScorer weps2007/test/truth_files/official_annotation history_cluster_results scorer_results -ALLMEASURES -AllInOne -OneInOne -Combined -average")
